cbf976744febf2709fc84fd747ccaf42d8ae0a8c
qiu <1462909477@qq.com>
2024-12-02 00:22:00 +0800

    feat(mr):lab 1

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,77 +1,120 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"io"
+	"io/fs"
+	"log"
+	"net/rpc"
+	"os"
+	"strconv"
+	"strings"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
-
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
 	// Your worker implementation here.
+	var args = &Args{WorkNum: -1}
+	// 规定map的num为奇数，reduce的num为偶数
+	for !args.IsFinish {
+		args = getJob(args)
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+		fileName := args.Job.FileName
+		if !args.Job.JobType {
+			file, err := os.ReadFile(fileName)
+			if err != nil {
+				continue
+			}
+			//执行mapf方法
+			keyValues := mapf(fileName, string(file))
+			//写文件
+			err = writeKV(keyValues, args.WorkNum, args.NReduce)
+			if err != nil {
+				return
+			}
+			//告诉master map执行完了
+			finish(args)
+		} else {
+			//如果是Reduce，获取map文件
+			file, err := os.Open(fileName)
+			if err != nil {
+				return
+			}
+			all, err := io.ReadAll(file)
+			if err != nil {
+				return
+			}
+			split := strings.Split(string(all), "\n")
+			m := make(map[string][]string)
+			for i := range split {
+				str := strings.TrimSpace(split[i])
+				kv := strings.Split(str, ":")
+				if m[kv[0]] == nil {
+					m[kv[0]] = make([]string, 0)
+				}
+				m[kv[0]] = append(m[kv[0]], kv[1])
+			}
+			//执行reduce
+			var s string
+			for kv := range m {
+				s = reducef(kv, m[kv])
+				oname := "mr-out-1"
+				ofile, _ := os.Create(oname)
+				fmt.Fprintf(ofile, "%v %v\n", kv, s)
+			}
+			finish(args)
+		}
 
-}
-
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
 	}
 }
 
-//
+func finish(args *Args) {
+	call("coordinator.Finish", args, nil)
+}
+
+func getJob(args *Args) *Args {
+	call("coordinator.GetJob", args, &args)
+	return args
+}
+
+type void struct{}
+
+var v void
+
+func writeKV(kv []KeyValue, workNum int, nReduce int) error {
+	set := make(map[string]void)
+
+	for i := range kv {
+		itoa := strconv.Itoa(ihash(kv[i].Key) % nReduce)
+		//防止并发问题？
+		var fileName = "/" + strconv.Itoa(workNum) + "-" + itoa + ".txt"
+		set[itoa] = v
+		err := os.WriteFile(fileName, []byte(kv[i].Key+":"+kv[i].Value), fs.ModeAppend)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
